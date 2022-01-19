1c0e4af86cd7c530402689b5ced72b65a7fc826b
AndrewJetLee <andrewjetlee777@gmail.com>
2022-01-19 16:51:22 -0600

    Todos successfully retrieved from database and rendered onto ejs file for /todos path

--- controllers/todos.js
+++ controllers/todos.js
@@ -3,8 +3,9 @@ const Todo = require("../models/Todo")
 module.exports = {
     getTodos: async (req, res) => {
         try {
-            let Todos = await Todo.find();
-            res.json(Todos);
+            let todos = await Todo.find();
+            let incompleteCount = await Todo.countDocuments({completed: false});
+            res.status(200).render("todos.ejs", {todos: todos, count: incompleteCount});
         } catch (err) {
             res.status(400).send(err);
         }
